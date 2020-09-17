ba02443bc455e4530f33d365c16190fc3aedae97
Olga Illarionova <ola.illari@gmail.com>
2020-09-17 11:40:38 -0700

    add requiremets promts

--- script.js
+++ script.js
@@ -17,7 +17,40 @@
 // This code handles:
 // * clicking the Generate Password
 // * writing the password to the screen
-//
+
+//I choose lowercase, uppercase, numeric, and/or special characters
+
+
+function generatePassword(){
+    let specials =  " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    let hasSpecial = false;
+    let hasLower = false;
+    let hasUpper = false;
+    let hasNumeric = false;
+    let len = 0;
+    while (!(hasLower||hasUpper||hasSpecial||hasNumeric)){
+      alert("You have to answer yes for at least one of following question")
+      hasLower = confirm("contains lowercase?");
+      hasUpper = confirm("contains uppercase??");
+      hasSpecial = confirm("contains special symbols?");
+      hasNumeric = confirm("contains digits?");
+    }
+    while ((!typeof(len)==='number')||(len<8)||(len>128)){
+      len = prompt("input password length between 8 and 128");
+    }
+  /// now we have all reqs and len ready
+    let numSpecial = hasSpecial? 1 : 0;
+    let numLower = hasLower? 1:0;
+    let numUpper = hasUpper? 1:0;
+    let numNumeric = hasNumeric? 1:0;
+
+    if (hasSpecial){
+
+    }
+
+    
+}
+
 
 var generateBtn = document.querySelector("#generate");
 
